01dd11923f23795f286ea8e831b3ae1cf7dba13d
devpiush <dev.bosepiush@gmail.com>
2025-02-14 16:09:03 +0530

    code: solved some pattern questiions

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 
+void print1(int n);
+void print2(int n);
+void print3(int n);
+void print4(int n);
+
 int main()
 {
-  int n;
-  std::cin >> n;
+  int t;
+  std::cin >> t;
 
+  for (auto i = 0; i < t; i++)
+  {
+    int n;
+    std::cin >> n;
+
+    std::cout << "Test Case: " << i + 1 << std::endl;
+    print3(n);
+    print4(n);
+    std::cout << std::endl;
+  }
+
+  return 0;
+}
+
+void print1(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j <= i; j++)
+    {
+      std::cout << (i + 1) << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+void print2(int n)
+{
   for (int i = 1; i <= n; i++)
   {
     for (int j = 1; j <= i; j++)
@@ -13,6 +45,52 @@ int main()
     }
     std::cout << std::endl;
   }
+}
+void print3(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    // space
+    for (int s = 0; s <= (n - i - 1); s++)
+    {
+      std::cout << " ";
+    }
 
-  return 0;
+    // star
+    for (int j = 0; j < ((i * 2) + 1); j++)
+    {
+      std::cout << "*";
+    }
+
+    // space
+    for (int s = 0; s <= (n - i - 1); s++)
+    {
+      std::cout << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+void print4(int n)
+{
+  for (int i = n; i >= 0; --i)
+  {
+    // space
+    for (int s = 0; s <= (n - i - 1); s++)
+    {
+      std::cout << " ";
+    }
+
+    // star
+    for (int j = 0; j < ((i * 2) + 1); j++)
+    {
+      std::cout << "*";
+    }
+
+    // space
+    for (int s = 0; s <= (n - i - 1); s++)
+    {
+      std::cout << " ";
+    }
+    std::cout << std::endl;
+  }
 }
