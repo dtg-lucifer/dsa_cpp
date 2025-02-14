5f911c4ab89cfd95f862e8f00f62f4437731e5d4
devpiush <dev.bosepiush@gmail.com>
2025-02-14 18:51:44 +0530

    code: solved two sum with hashmap

---
+++ leetcode/two_sum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+class Solution
+{
+public:
+  std::vector<int> twoSum(std::vector<int> &nums, int target)
+  {
+    std::unordered_map<int, std::pair<int, int>> map;
+
+    for (auto it = nums.begin(); it != nums.end(); ++it)
+    {
+      int index = std::distance(nums.begin(), it);
+
+      if (map.find(*it) != map.end())
+      {
+        int diff_index = map[*it].first;
+        return std::vector<int>{diff_index, index};
+      }
+
+      int diff = target - (*it);
+      map.insert({diff, {index, *it}});
+    }
+
+    return std::vector<int>{};
+  }
+};
