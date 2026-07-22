b8a5eebe9994518457a93b96eb7ea744a3741d1e
dtg-lucifer <piushbasu@gmail.com>
2026-07-22 15:24:30 +0530

    code: solved 2 questions for july 22nd 2026

---
+++ leetcode/328_odd_even_linked_list.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+
+        ListNode* f = head;       // odd pointer
+        ListNode* s = head->next; // even pointer
+        ListNode* eh = s;         // even head
+
+        while (s != nullptr && s->next != nullptr) {
+            f->next = s->next; // assign the odd next
+            f = f->next;       // forward the odd pointer
+
+            s->next = s->next->next; // assign the even next
+            s = s->next;             // forward the even pointer
+        }
+
+        f->next = eh;
+        return head;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    Solution sol;
+    ListNode* result = sol.oddEvenList(head);
+
+    std::cout << "Result: ";
+    while (result != nullptr) {
+        std::cout << result->val << " ";
+        result = result->next;
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
